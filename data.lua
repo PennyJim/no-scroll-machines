6e8a72ca2a7b1a3b267934aa55db66bb5d0ccb3e
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-10-10 10:27:22 -0600

    Make mod

---
+++ data.lua
@@ -0,0 +1,6 @@
+local styles = data.raw["gui-style"]["default"]
+
+styles.machine_slots_scroll_pane.maximal_height = nil
+styles.machine_ingredients_scroll_pane.maximal_height = nil
+styles.machine_outputs_scroll_pane.maximal_height = nil
+styles.module_inventory_scroll_pane.maximal_height = nil
